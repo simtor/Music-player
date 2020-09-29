3c2c5db9983ad6b9cffd51b711bed7c78603cc94
simtor <simone.ram@ada.ac.uk>
2020-09-29 16:45:51 +0100

    building

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 const { exec } = require("child_process");
-
-exec("afplay Example.mp3", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.log("Finished")
+const readline = require('readline');
+const rl = readline.createInterface({
+  input : process.stdin, 
+  output : process.stdout
 });
+// function inputHandler(answer) {
+//   if (answer === "Q") {
+//     rl.close()
+//     return null
+//   }
+//   if (result) {
+//     console.log("You Product has been found", answer)
+//     rl.question("Input your medical licence number or Q to quit: ", inputHandler)
+//     return result
+//   } else {
+//     rl.write("The Medical licence number was not found :(\n")
+//     rl.question("Input your medical licence number or Q to quit: ", inputHandler)
+//   }
+// }
+// rl.question("Input your medical licence number or Q to quit: ", inputHandler)
 
 
+
+function playerSelector(selectedSong){
+    if(selectedSong== 1){
+        exec("afplay Example.mp3")
+        console.log("started")
+    }else if (selectedSong== 2){
+        exec("afplay Left.mp3");
+    }else{
+        console.log("Sorry")
+        rl.close()
+    }
+    rl.question("Input song: ", playerSelector)
+    // rl.question("select song: ", selectedSong);
+    // return selectedSong
+    
+}
+playerSelector()
+
+
+
+
+
+
+
+
+
+// exec("afplay Example.mp3", (error, stdout, stderr) => {
+//     console.log(`stdout: ${stdout}`);
+//     console.log("Finished")
+// });
