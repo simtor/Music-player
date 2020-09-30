ce1fc2b35ae69337622cee7563c482ef99feeb3f
simtor <simone.ram@ada.ac.uk>
2020-09-30 21:55:32 +0100

    Volume added

--- index.js
+++ index.js
@@ -4,21 +4,6 @@ const rl = readline.createInterface({
   input : process.stdin, 
   output : process.stdout
 });
-// function inputHandler(answer) {
-//   if (answer === "Q") {
-//     rl.close()
-//     return null
-//   }
-//   if (result) {
-//     console.log("You Product has been found", answer)
-//     rl.question("Input your medical licence number or Q to quit: ", inputHandler)
-//     return result
-//   } else {
-//     rl.write("The Medical licence number was not found :(\n")
-//     rl.question("Input your medical licence number or Q to quit: ", inputHandler)
-//   }
-// }
-// rl.question("Input your medical licence number or Q to quit: ", inputHandler)
 
 console.log("songs list: 1.Example 2.Left")
 
@@ -35,28 +20,22 @@ function playerSelector(selectedSong){
     }else if(selectedSong==='Q'){
         rl.close()
     }else if(selectedSong==='V'){
-        exec("afplay Example.mp3 -v 0");
+        rl.question("volume: ", volume)
+        exec(`afplay Example.mp3 -v ${volume}`);
         rl.question("Input song: ", playerSelector)
     }else{
         rl.question("Input song: ", playerSelector)
     }
-    // rl.question("select song: ", selectedSong);
-    // return selectedSong
-    
 }
-playerSelector()
-
-
 
 
 
 // exec("sudo osascript -e set Volume 0 ")
 
 
-
-
-
 // exec("afplay Example.mp3", (error, stdout, stderr) => {
 //     console.log(`stdout: ${stdout}`);
 //     console.log("Finished")
 // });
+
+
